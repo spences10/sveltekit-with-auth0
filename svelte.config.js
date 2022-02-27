9f3f9c951c757a5f2952faea826c2b338aaf6cd9
Scott Spence <spences10apps@gmail.com>
2022-02-27 08:39:49 +0000

    remove kit.target

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,6 @@ import adapter from '@sveltejs/adapter-vercel'
 
 const config = {
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
     adapter: adapter(),
   },
 }
